b597c5b3c904271ddd08cd0bae8c7eee91a92c62
alexandru_fako <alexandru_fako@epam.com>
2022-09-16 12:19:59 +0300

    todo details

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable no-unused-vars */
 import React, {useState, useRef, useEffect} from "react"
 import {useNavigate} from 'react-router-dom';
